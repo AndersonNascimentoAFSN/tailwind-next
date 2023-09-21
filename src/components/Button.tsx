1775c421a85abc4bedb0c318b5466cf06fef551e
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-09-21 14:11:18 -0300

    feat: add button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,23 +1,16 @@
 import { ComponentProps } from 'react'
-import { CheckCircleIcon } from '@heroicons/react/20/solid'
-import { twMerge } from 'tailwind-merge'
 import { tv, VariantProps } from 'tailwind-variants'
 
 const button = tv({
-  base: 'bg-zinc-50 font-medium rounded text-sm text-zinc-900 hover:bg-zinc-200',
+  base: 'rounded-lg px-4 py-2 text-sm font-semibold shadow-sm',
   variants: {
-    size: {
-      default: 'h-10  px-4',
-      sm: 'h-8  px-3',
-      xs: 'h-6  px-2 text-sx',
-    },
-    success: {
-      true: 'bg-emerald-400 hover:bg-emerald-600',
+    variant: {
+      primary: 'bg-violet-600 text-white hover:bg-violet-700',
+      secondary: 'border border-zinc-300 text-zinc-700 hover:bg-zinc-50',
     },
   },
   defaultVariants: {
-    size: 'default',
-    success: false,
+    variant: 'primary',
   },
 })
 
@@ -25,14 +18,42 @@ export interface ButtonProps
   extends ComponentProps<'button'>,
     VariantProps<typeof button> {}
 
-// tailwind variant
-export function Button({ size, success, className, ...props }: ButtonProps) {
-  return (
-    <Button className={button({ size, success, className })} {...props}>
-      {success ? <CheckCircleIcon className="h-4 w-4" /> : props.children}
-    </Button>
-  )
+export function Button({ variant, className, ...props }: ButtonProps) {
+  return <button {...props} className={button({ variant, className })} />
 }
+
+// import { CheckCircleIcon } from '@heroicons/react/20/solid'
+// import { twMerge } from 'tailwind-merge'
+// const button = tv({
+//   base: 'bg-zinc-50 font-medium rounded text-sm text-zinc-900 hover:bg-zinc-200',
+//   variants: {
+//     size: {
+//       default: 'h-10  px-4',
+//       sm: 'h-8  px-3',
+//       xs: 'h-6  px-2 text-sx',
+//     },
+//     success: {
+//       true: 'bg-emerald-400 hover:bg-emerald-600',
+//     },
+//   },
+//   defaultVariants: {
+//     size: 'default',
+//     success: false,
+//   },
+// })
+
+// export interface ButtonProps
+//   extends ComponentProps<'button'>,
+//     VariantProps<typeof button> {}
+
+// // tailwind variant
+// export function Button({ size, success, className, ...props }: ButtonProps) {
+//   return (
+//     <Button className={button({ size, success, className })} {...props}>
+//       {success ? <CheckCircleIcon className="h-4 w-4" /> : props.children}
+//     </Button>
+//   )
+// }
 
 // data-success e twMerge
 // export function Button({ success = false, className, ...props }: ButtonProps) {
